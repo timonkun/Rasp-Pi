cc34e3aa5c4324471b5b4f520eef9a6f1d465de9
Kun Ling <timonkun@gmail.com>
2013-09-28 00:00:32 +0800

    improve temprature.py

--- temprature/temprature.py
+++ temprature/temprature.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python2.7  
+# -*- coding: utf-8 -*-
+
 import os
 import time
 import sys
@@ -7,6 +9,7 @@ import weibo_func
 import string
 
 count = 0
+WEIBO_FLAG = 0  #发微博还是传yeelink
   
 while True:
 	try:
@@ -17,25 +20,44 @@ while True:
 		cur_t_line = p.readline()
 		cur_t = string.atof(cur_t_line)
 	
+		# 错误值，PASS
 		if cur_h == 0.0 and cur_t == 0.0:
 			print 'Value error.'
 			continue
 
-		line_h = ' Humidity: ' + str(cur_h) + '%'
-		line_t = ' Temprature: ' + str(cur_t) + '*C'
+		# 发微博
+		if WEIBO_FLAG == 1:
+			line_h = ' Humidity: ' + str(cur_h) + '%'
+			line_t = ' Temprature: ' + str(cur_t) + '*C'
 
-		timeStr = time.asctime()
-		count += 1
-		countStr = str(count) + '. '
+			timeStr = time.asctime()
+			count += 1
+			countStr = str(count) + '. '
 
-		print countStr + timeStr
-		print line_t
-		print line_h
-		lineStr = line_t + line_h
-		weibo_func.send_weibo(weibo_type=weibo_func.TEMPRATURE_WEIBO, \
-			c=countStr, t=timeStr, h=lineStr)
-		print "weibo sending done."
-		time.sleep(3600)
+			print countStr + timeStr
+			print line_t
+			print line_h
+			lineStr = line_t + line_h
+
+			weibo_func.send_weibo(weibo_type=weibo_func.TEMPRATURE_WEIBO, \
+				c=countStr, t=timeStr, h=lineStr)
+			print "weibo sending done."
+			time.sleep(3600)
+		else:	#传yeelink
+			yeelink_t_str = str(cur_t)
+			yeelink_cmd_t = '''curl --request POST --data '{"value":''' + yeelink_t_str + \
+			'''}' --header "U-ApiKey:6640382536cf31808bb94e83fe4e8f4c" \
+			http://api.yeelink.net/v1.0/device/4014/sensor/8028/datapoints'''
+			#print yeelink_cmd_t
+			p= os.popen(yeelink_cmd_t, 'r')
+
+			yeelink_h_str = str(cur_h)
+			yeelink_cmd_h = '''curl --request POST --data '{"value":''' + yeelink_h_str + \
+			'''}' --header "U-ApiKey:6640382536cf31808bb94e83fe4e8f4c" \
+			http://api.yeelink.net/v1.0/device/4014/sensor/8031/datapoints'''
+			#print yeelink_cmd_h
+			p= os.popen(yeelink_cmd_h, 'r')
+			time.sleep(300)
 
 	except KeyboardInterrupt:  
 		print 'Ctrl+C to quit.'       # CTRL+C exit
